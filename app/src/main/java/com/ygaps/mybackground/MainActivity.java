5a69676e676222cbd60157aa7a7ff87e61fad0b6
TPL <trphloc@gmail.com>
2017-06-20 11:17:56 +0700

    + chuẩn bị thanh loading

--- app/src/main/java/com/ygaps/mybackground/MainActivity.java
+++ app/src/main/java/com/ygaps/mybackground/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ygaps.mybackground;
 
+import android.app.ProgressDialog;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +10,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressDialog processDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar); 
+        setSupportActionBar(toolbar);
+        processDialog = new ProgressDialog(this);
+        processDialog.setCancelable(false);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        processDialog.dismiss();
     }
 
     @Override
