f7737f65e4714971d1ed170d44ac1f64677e5f52
TPL <trphloc@gmail.com>
2017-06-27 10:06:18 +0700

    - Màn hình lịch: hiển thị nút back

--- app/src/main/java/com/ygaps/mybackground/CalendarActivity.java
+++ app/src/main/java/com/ygaps/mybackground/CalendarActivity.java
@@ -1,10 +1,12 @@
 package com.ygaps.mybackground;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class CalendarActivity extends AppCompatActivity {
@@ -15,6 +17,13 @@ public class CalendarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
 }
